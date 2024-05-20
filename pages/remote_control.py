490abf91e0902ba5770567547db2d87fc4f0e24a
zbuhrer <zbuhrer@gmail.com>
2024-05-19 22:36:07 -0500

    modified:   pages/remote_control.py

--- pages/remote_control.py
+++ pages/remote_control.py
@@ -1,41 +1,59 @@
 import flet as ft
 import json, requests
+import math
+from math import pi
+from utils.extras import *
 
 class RemoteControlPage(ft.Container):
   def __init__(self,switch_page):
     super().__init__()
+    self.expand=True
     self.switch_page = switch_page    
     self.content = ControlPanel()
     
 class ControlPanel(ft.Container):
     def __init__(self):
         super().__init__()
-        
+        self.expand = True
         self.dpadwidget = ft.Column(
-           rotate=45,
            controls=[
-                ft.Row([ft.IconButton(icon=ft.icons.QUESTION_MARK), ft.IconButton(icon=ft.icons.QUESTION_MARK)]),
-                ft.Row([ft.IconButton(icon=ft.icons.QUESTION_MARK), ft.IconButton(icon=ft.icons.QUESTION_MARK)])
+                ft.Row([ft.IconButton(icon=ft.icons.ARROW_UPWARD_OUTLINED,rotate=pi/-4,tooltip='Forward'), ft.IconButton(icon=ft.icons.KEYBOARD_ARROW_RIGHT,rotate=pi/-4,tooltip='Turn Right')]),
+                ft.Row([ft.IconButton(icon=ft.icons.KEYBOARD_ARROW_LEFT,rotate=pi/-4,tooltip='Turn Left'), ft.IconButton(icon=ft.icons.ARROW_DOWNWARD_OUTLINED,rotate=pi/-4,tooltip='Backward')]),
             ])
         self.headwidget = ft.Column(
            controls=[
-                ft.Row([ft.IconButton(icon=ft.icons.QUESTION_MARK)]),
-                ft.Row([ft.IconButton(icon=ft.icons.QUESTION_MARK)])
+                ft.Row([ft.IconButton(icon=ft.icons.ARROW_UPWARD_ROUNDED)]),
+                ft.Row([ft.IconButton(icon=ft.icons.ARROW_DOWNWARD_ROUNDED)])
             ])
         self.armwidget = ft.Column(
            controls=[
-                ft.Row([ft.IconButton(icon=ft.icons.QUESTION_MARK)]),
-                ft.Row([ft.IconButton(icon=ft.icons.QUESTION_MARK)])
+                ft.Row([ft.IconButton(icon=ft.icons.ARROW_UPWARD_ROUNDED)]),
+                ft.Row([ft.IconButton(icon=ft.icons.ARROW_DOWNWARD_ROUNDED)])
             ])
         
-        self.controlstack = ft.Container(content=ft.Row(controls=[
-            self.dpadwidget,
-            ft.Column(adaptive=True),
-            self.headwidget,
-            self.armwidget]))
+        self.controlstack = ft.Row(
+            expand=True,
+            controls=[
+                    self.dpadwidget,
+                    self.headwidget,
+                    self.armwidget
+                    ],
+            )
         self.wheel_control = None
         self.lift_head_control = None
         self.content = self.controlstack
+
+        self.init_helper()
+
+    def init_helper(self):
+        self.dpadwidget.rotate=pi/4
+        self.dpadwidget.alignment=ft.MainAxisAlignment.END
+        self.headwidget.alignment=ft.MainAxisAlignment.END
+        self.armwidget.alignment=ft.MainAxisAlignment.END
+        self.controlstack.alignment=ft.MainAxisAlignment.SPACE_EVENLY
+
+        
+        return
         
 
         # self.content=ft.Column(controls=[
